05b1c6874ec397ed7f40954323309e908fec8fdd
Russell Sotiropoulos <35404334+russotiro@users.noreply.github.com>
2023-04-18 19:34:29 -0400

    output generated

--- api/api.py
+++ api/api.py
@@ -1,8 +1,8 @@
-import time
 from flask import Flask, request, Response, jsonify
 from flask_cors import CORS
 import os
 from pyth import transpiler
+import pathlib
 
 
 app = Flask(__name__)
@@ -14,10 +14,19 @@ def flask_landing_page():
 
 @app.route('/transpile', methods=['POST'])
 def flask_transpile_page():
+    # musicode -> lilypond
     user_input = request.get_json()['user_input']
-    midi = True
+    is_midi = True
     if request.get_json()['generate'] == "PDF":
-        midi = False
+        is_midi = False
     # TODO: report any errors from line below
-    ly_code = transpiler.transpile(user_input=user_input, midi=midi)
-    return {'ly_code': ly_code, 'midi': midi}
+    ly_code = transpiler.transpile(user_input=user_input, midi=is_midi)
+    with open('my_song.ly', 'w') as f:
+        f.write(ly_code)
+
+    # lilypond -> PDF/MIDI
+    ly_exec = pathlib.Path(__file__).parents[1]
+    ly_exec = ly_exec.joinpath('lilypond-2.24.1', 'bin', 'lilypond.exe').resolve()
+    os.system('"' + str(ly_exec) + '" my_song.ly')
+
+    return {'midi': is_midi}
